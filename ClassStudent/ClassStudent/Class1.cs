98ff5fcf0d77b8caa8714ec0ce5382129e710e28
andrtu2 <andreica_tudor@yahoo.com>
2015-11-08 16:02:42 +0200

    Added new project + student class

---
+++ ClassStudent/ClassStudent/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudent
+{
+    public class Student
+    {
+        private string Nume;
+        private string Prenume;
+        private int CNP;
+        private int NrMatricol;
+
+        public Student(string Nume, string Prenume, int CNP,int NrMatricol)
+        {
+            this.Nume = Nume;
+            this.Prenume = Prenume;
+            this.CNP = CNP;
+            this.NrMatricol = NrMatricol;
+        }
+    }
+}
